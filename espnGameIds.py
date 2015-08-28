3d7b4abe6f577ad22751b28a266c39c9e782de5f
bdbozyk <brett.bozyk@gmail.com>
2015-08-28 00:14:25 -0400

    finished espnGameIds.py

--- espnGameIds.py
+++ espnGameIds.py
@@ -1,13 +1,30 @@
+### VARIABLES NEEDED ----------------------------------------------------------
+#   dataDirectory - LOCATION FOR OUTPUT DATASET(S)
+#            year - YEAR FOR THE GAMEIDS
+#------------------------------------------------------------------------------
+
+### PROGRAM SUMMARY -----------------------------------------------------------
+#   THIS PROGRAM TAKES THE <year>, GOES TO ESPN.COM, AND GRABS ALL THE GAMEIDS
+#   FOR THAT year. IT WRITES THEM IN A TABLE WITH THE FOLLOWING:
+#      {SOURCE_URL,YEAR,SEASONTYPE,WEEK,GAMEID}.
+#   IT FINISHES BY WRITING THIS DATASET TO <dataDirectory> USING THE FOLLOWING
+#   NAMING SCHEME:
+#      <dataDirectory>/gameSummary/gameSummary_<year>.csv
+#------------------------------------------------------------------------------
+
+### VARIABLE SPECIFICATION ----------------------------------------------------
+dataDirectory = '/home/brett/espnNFL/'
+year = 2015
+#------------------------------------------------------------------------------
+
 import urllib
 import pandas
 import re
 import os
 
-os.chdir('/)
 
-
+os.chdir(dataDirectory)
 dfs = []
-year = 2014
 keys = []
 for i in xrange(1,6):
     keys.append([1,i,year])
@@ -16,9 +33,7 @@ for i in xrange(1,18):
 for i in xrange(1,6):
     keys.append([3,i,year])
 
-
-for seasontype,week,year in keys:
-        
+for seasontype,week,year in keys:      
     url = 'http://espn.go.com/nfl/scoreboard?year='+str(year)+'&seasontype='+str(seasontype)+'&week='+str(week)
     html = urllib.urlopen(url).read()
     
@@ -28,47 +43,26 @@ for seasontype,week,year in keys:
     elif seasontype == 2:
         seasontype = 'REG'
     else:
-        seasontype = 'POST'
-    
-    
+        seasontype = 'POST'   
     # get all the gameIds for the week
     gameIdsWork = re.findall(r'/boxscore\?gameId=.*?"',work)
     gameIds = []
     for gameId in gameIdsWork:
         gameIds.append(re.sub("[^0-9]","",gameId))
-        gameIds.append(re.sub("[^0-9]","",gameId))
-    # get all the total scores
-    totalScoresWork = re.findall(r'"score":".*?"',work)
-    totalScores = []
-    for totalScore in totalScoresWork:
-        totalScores.append(re.sub("[^0-9]","",totalScore))
-    # get all the team names
-    teamsWork = re.findall(r'shortDisplayName":".*?"',work)
-    teams = []
-    for team in teamsWork:
-        teams.append(team[19:-1])
-    # get all the scores by quarter
-    scoresWork = re.findall(r'"linescores":\[.*?\]',work)
-    scores = []
-    for score in scoresWork:
-        qtrsWork = re.findall(r'"value":.*?}',score)
-        qtrs = []
-        for qtr in qtrsWork:
-            qtrs.append(re.sub(r'[^0-9]',"",qtr))
-        if len(qtrs) == 4:
-            qtrs.append('0')
-        scores.append(qtrs)
+   
     rows = []
-    for i in range(len(gameIds)):
-        rows.append([year,seasontype,week,gameIds[i],teams[i],totalScores[i],scores[i][0],scores[i][1],scores[i][2],scores[i][3],scores[i][4]])
+    for gameId in gameIds:
+         rows.append([url.upper(),year,seasontype,week,gameId])
+   
     if len(rows) == 0:
-        print 'Did nothing for this week.'
+         print 'Did nothing for this week.'
     else:
-        df = pandas.DataFrame(rows)
-        df.columns = ['SEASON_YEAR','SEASON_TYPE','WEEK','ESPN_GAME_ID','TEAM','TOTAL_PTS','Q1_PTS','Q2_PTS','Q3_PTS','Q4_PTS','OT_PTS']
-        dfs.append(df)
-        print str(year)+' '+seasontype+' '+str(week)+' is done.'
+         df = pandas.DataFrame(rows)
+         df.columns = ['SOURCE','SEASON_YEAR','SEASON_TYPE','WEEK','GAME_ID']
+         dfs.append(df)
+         print str(year)+' '+seasontype+' '+str(week)+' is done.'
   
+pandas.concat(dfs).to_csv(os.getcwd()+'/gameSummary/gameSummary_'+str(year)+'.csv',index=False)
     
     
     
