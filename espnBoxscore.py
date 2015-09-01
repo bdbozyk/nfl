ac379dd8da05e83f988c914fb8c39e584b902287
bdbozyk <brett.bozyk@gmail.com>
2015-09-01 00:45:28 -0400

    updated espnBoxscore.py

--- espnBoxscore.py
+++ espnBoxscore.py
@@ -176,7 +176,7 @@ rushing = pandas.concat([awayRushing,homeRushing])
 rushing.to_csv(os.getcwd()+'/rushing/rushing_'+gameId+'.csv',index=False)
     
 ###-------------------------------------------------------------------------###
-###   START OF AWAY PASING   -----------------------------------------------###
+###   START OF AWAY RECEIVING   --------------------------------------------###
 ###-------------------------------------------------------------------------###
 string5 = strings[5]
 junk = re.findall(r'>.*?<',string5)
@@ -189,13 +189,37 @@ del(junk2[:7],junk2[-7:])
 rows = []
 for i in xrange(int(len(junk2)/7.)):
     rows.append(junk2[i*7:(i+1)*7])
-awayPassingFrame = pandas.DataFrame(rows)
-
-
-
-
-homeRushingFrame.columns = ['PLAYER','CARRIES','YDS','YDS_PER_CARRY','TD','LONG']    
-junk3 = re.findall(r'id/.*?<',string4)
+awayReceivingFrame = pandas.DataFrame(rows)
+awayReceivingFrame.columns = ['PLAYER','REC','YDS','AVG_YDS_REC','TD','REC_LONG','TGTS']
+junk3 = re.findall(r'id/.*?<',string5)
+rows = []
+for thing in junk3:
+    temp = thing.split('"')
+    rows.append([re.sub('[^0-9]','',temp[0]),temp[1].translate(None,'><').upper()])
+playerFrame = pandas.DataFrame(rows)
+playerFrame.columns = ['PLAYER_ID','PLAYER']
+playerFrame['GAME_ID'] = gameId
+playerFrame['HOME_FG'] = 0
+awayReceiving = pandas.merge(left = playerFrame, right = awayReceivingFrame, on = 'PLAYER')
+awayReceiving = awayReceiving[['GAME_ID','PLAYER_ID','PLAYER','HOME_FG','REC','YDS','AVG_YDS_REC','TD','REC_LONG','TGTS']]
+   
+###-------------------------------------------------------------------------###
+###   START OF HOME RECEIVING   --------------------------------------------###
+###-------------------------------------------------------------------------###
+string6 = strings[6]
+junk = re.findall(r'>.*?<',string6)
+junk2 = []
+for thing in junk:
+    temp = thing.translate(None,'><')
+    if temp != '':
+        junk2.append(temp.upper())
+del(junk2[:7],junk2[-7:])
+rows = []
+for i in xrange(int(len(junk2)/7.)):
+    rows.append(junk2[i*7:(i+1)*7])
+homeReceivingFrame = pandas.DataFrame(rows)
+homeReceivingFrame.columns = ['PLAYER','REC','YDS','AVG_YDS_REC','TD','REC_LONG','TGTS']
+junk3 = re.findall(r'id/.*?<',string6)
 rows = []
 for thing in junk3:
     temp = thing.split('"')
@@ -204,9 +228,195 @@ playerFrame = pandas.DataFrame(rows)
 playerFrame.columns = ['PLAYER_ID','PLAYER']
 playerFrame['GAME_ID'] = gameId
 playerFrame['HOME_FG'] = 1
-homeRushing = pandas.merge(left = playerFrame, right = homeRushingFrame, on = 'PLAYER')
-homeRushing = homeRushing[['GAME_ID','PLAYER_ID','PLAYER','HOME_FG','CARRIES','YDS','YDS_PER_CARRY','TD','LONG']]
-   
+homeReceiving = pandas.merge(left = playerFrame, right = homeReceivingFrame, on = 'PLAYER')
+homeReceiving = homeReceiving[['GAME_ID','PLAYER_ID','PLAYER','HOME_FG','REC','YDS','AVG_YDS_REC','TD','REC_LONG','TGTS']]
     
+###-------------------------------------------------------------------------###
+###   COMBINE HOME AND AWAY RECEIVING  -------------------------------------###
+###-------------------------------------------------------------------------###
+receiving = pandas.concat([awayReceiving,homeReceiving])
+receiving.to_csv(os.getcwd()+'/receiving/receiving_'+gameId+'.csv',index=False)
+
+###-------------------------------------------------------------------------###
+###   START OF AWAY FUMBLES   ----------------------------------------------###
+###-------------------------------------------------------------------------###
+string7 = strings[7]
+junk = re.findall(r'>.*?<',string7)
+junk2 = []
+for thing in junk:
+    temp = thing.translate(None,'><')
+    if temp != '':
+        junk2.append(temp.upper())
+del(junk2[:4],junk2[-4:])
+rows = []
+for i in xrange(int(len(junk2)/4.)):
+    rows.append(junk2[i*4:(i+1)*4])
+if len(rows) == 0:
+    rows = [['NONE',0,0,0]]
+awayFumbleFrame = pandas.DataFrame(rows)
+awayFumbleFrame.columns = ['PLAYER','FUM','LOST','REC']
+junk3 = re.findall(r'id/.*?<',string7)
+rows = []
+for thing in junk3:
+    temp = thing.split('"')
+    rows.append([re.sub('[^0-9]','',temp[0]),temp[1].translate(None,'><').upper()])
+if len(rows) == 0:
+    rows = [[-1,'NONE']]
+playerFrame = pandas.DataFrame(rows)
+playerFrame.columns = ['PLAYER_ID','PLAYER']
+playerFrame['GAME_ID'] = gameId
+playerFrame['HOME_FG'] = 0
+awayFumble = pandas.merge(left = playerFrame, right = awayFumbleFrame, on = 'PLAYER')
+awayFumble = awayFumble[['GAME_ID','PLAYER_ID','PLAYER','HOME_FG','FUM','LOST','REC']]
+
+###-------------------------------------------------------------------------###
+###   START OF HOME FUMBLES   ----------------------------------------------###
+###-------------------------------------------------------------------------###
+string8 = strings[8]
+junk = re.findall(r'>.*?<',string8)
+junk2 = []
+for thing in junk:
+    temp = thing.translate(None,'><')
+    if temp != '':
+        junk2.append(temp.upper())
+del(junk2[:4],junk2[-4:])
+rows = []
+for i in xrange(int(len(junk2)/4.)):
+    rows.append(junk2[i*4:(i+1)*4])
+if len(rows) == 0:
+    rows = [['NONE',0,0,0]]
+homeFumbleFrame = pandas.DataFrame(rows)
+homeFumbleFrame.columns = ['PLAYER','FUM','LOST','REC']
+junk3 = re.findall(r'id/.*?<',string8)
+rows = []
+for thing in junk3:
+    temp = thing.split('"')
+    rows.append([re.sub('[^0-9]','',temp[0]),temp[1].translate(None,'><').upper()])
+if len(rows) == 0:
+    rows = [[-1,'NONE']]
+playerFrame = pandas.DataFrame(rows)
+playerFrame.columns = ['PLAYER_ID','PLAYER']
+playerFrame['GAME_ID'] = gameId
+playerFrame['HOME_FG'] = 1
+homeFumble = pandas.merge(left = playerFrame, right = homeFumbleFrame, on = 'PLAYER')
+homeFumble = homeFumble[['GAME_ID','PLAYER_ID','PLAYER','HOME_FG','FUM','LOST','REC']]
     
+###-------------------------------------------------------------------------###
+###   COMBINE HOME AND AWAY PASSING  ---------------------------------------###
+###-------------------------------------------------------------------------###
+fumbles = pandas.concat([awayFumble,homeFumble])
+fumbles.to_csv(os.getcwd()+'/fumbles/fumbles_'+gameId+'.csv',index=False)
+
+###-------------------------------------------------------------------------###
+###   START OF AWAY DEFENSE   ----------------------------------------------###
+###-------------------------------------------------------------------------###
+string = strings[9]
+junk = re.findall(r'>.*?<',string)
+junk2 = []
+for thing in junk:
+    temp = thing.translate(None,'><')
+    if temp != '':
+        junk2.append(temp.upper())
+del(junk2[:8],junk2[-8:])
+rows = []
+for i in xrange(int(len(junk2)/8.)):
+    rows.append(junk2[i*8:(i+1)*8])
+frame = pandas.DataFrame(rows)
+frame.columns = ['PLAYER','TOT','SOLO','SACKS','TFL','PD','QB_HITS','TD']
+junk3 = re.findall(r'id/.*?<',string)
+rows = []
+for thing in junk3:
+    temp = thing.split('"')
+    rows.append([re.sub('[^0-9]','',temp[0]),temp[1].translate(None,'><').upper()])
+playerFrame = pandas.DataFrame(rows)
+playerFrame.columns = ['PLAYER_ID','PLAYER']
+playerFrame['GAME_ID'] = gameId
+playerFrame['HOME_FG'] = 0
+frame = pandas.merge(left = playerFrame, right = frame, on = 'PLAYER')
+awayDefense = frame[['GAME_ID','PLAYER_ID','PLAYER','HOME_FG','TOT','SOLO','SACKS','TFL','PD','QB_HITS','TD']]
+
+###-------------------------------------------------------------------------###
+###   START OF HOME DEFENSE   ----------------------------------------------###
+###-------------------------------------------------------------------------###
+string = strings[10]
+junk = re.findall(r'>.*?<',string)
+junk2 = []
+for thing in junk:
+    temp = thing.translate(None,'><')
+    if temp != '':
+        junk2.append(temp.upper())
+del(junk2[:8],junk2[-8:])
+rows = []
+for i in xrange(int(len(junk2)/8.)):
+    rows.append(junk2[i*8:(i+1)*8])
+frame = pandas.DataFrame(rows)
+frame.columns = ['PLAYER','TOT','SOLO','SACKS','TFL','PD','QB_HITS','TD']
+junk3 = re.findall(r'id/.*?<',string)
+rows = []
+for thing in junk3:
+    temp = thing.split('"')
+    rows.append([re.sub('[^0-9]','',temp[0]),temp[1].translate(None,'><').upper()])
+playerFrame = pandas.DataFrame(rows)
+playerFrame.columns = ['PLAYER_ID','PLAYER']
+playerFrame['GAME_ID'] = gameId
+playerFrame['HOME_FG'] = 1
+frame = pandas.merge(left = playerFrame, right = frame, on = 'PLAYER')
+homeDefense = frame[['GAME_ID','PLAYER_ID','PLAYER','HOME_FG','TOT','SOLO','SACKS','TFL','PD','QB_HITS','TD']]
+
+###-------------------------------------------------------------------------###
+###   COMBINE HOME AND AWAY DEFENSE  ---------------------------------------###
+###-------------------------------------------------------------------------###
+defense = pandas.concat([awayDefense,homeDefense])
+defense.to_csv(os.getcwd()+'/defense/defense_'+gameId+'.csv',index=False)
+
+
+
+
+
+
+
+###-------------------------------------------------------------------------###
+###   START OF AWAY INTERCEPTIONS   ----------------------------------------###
+###-------------------------------------------------------------------------###
+string = strings[11]
+junk = re.findall(r'>.*?<',string)
+junk2 = []
+for thing in junk:
+    temp = thing.translate(None,'><')
+    if temp != '':
+        junk2.append(temp.upper())
+del(junk2[:4],junk2[-4:])
+rows = []
+for i in xrange(int(len(junk2)/4.)):
+    rows.append(junk2[i*4:(i+1)*4])
+if len(rows) == 0:
+    rows = [['NONE',0,0,0]]
+frame = pandas.DataFrame(rows)
+frame.columns = ['PLAYER','INT','YDS','TD']
+junk3 = re.findall(r'id/.*?<',string)
+rows = []
+for thing in junk3:
+    temp = thing.split('"')
+    rows.append([re.sub('[^0-9]','',temp[0]),temp[1].translate(None,'><').upper()])
+if len(rows) == 0:
+    rows = [[-1,'NONE']]
+playerFrame = pandas.DataFrame(rows)
+playerFrame.columns = ['PLAYER_ID','PLAYER']
+playerFrame['GAME_ID'] = gameId
+playerFrame['HOME_FG'] = 0
+frame = pandas.merge(left = playerFrame, right = frame, on = 'PLAYER')
+awayFumble = awayFumble[['GAME_ID','PLAYER_ID','PLAYER','HOME_FG','FUM','LOST','REC']]
+
+
+
+
+###
+###   1. teams
+###   2. passing
+###   3. rushing
+###   4. receiving
+###   5. fumbles
+###   6. defense
+
+
     
